052b6e30e07ab7ed4255925057ff7eefb7b0f2f7
nikolaivkovic01 <nikola.ivkovic.contact@gmail.com>
2025-10-19 23:10:02 +0200

    chore: Remove empty test file

--- HrPlatform.Tests/obj/Debug/net10.0/HrPlatform.Tests.AssemblyInfo.cs
+++ HrPlatform.Tests/obj/Debug/net10.0/HrPlatform.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HrPlatform.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2b1f54e5a1ae2fa2f669446a7bea55852a7549")]
 [assembly: System.Reflection.AssemblyProductAttribute("HrPlatform.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HrPlatform.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
